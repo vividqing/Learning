d30c6ccbb77b93c70afc2e459fb1b033a8938f33
devld <devlduio@gmail.com>
2017-06-05 08:24:23 -0500

    Update ninenine.java
    对齐代码，删除无用代码。

--- Nine/A-01/src/ninenine.java
+++ Nine/A-01/src/ninenine.java
@@ -1,24 +1,19 @@
-import java.util.Scanner;
+// import java.util.Scanner; // 这个导包没用
 
 public class ninenine {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // Scanner s = new Scanner(System.in); // 这个Scanner 用不上
     
-    int i=0,j=0,k=0;
-    System.out.print("九九乘法表\n");
-    for(i=1;i<10;i++){
-    	for(j=1;j<=i;j++){
-    		k=i*j;
-    		System.out.print(""+i+"×"+j+"="+k+"\t");
-    	}
-    	System.out.println("");
-    	
+        int i=0,j=0,k=0;
+        System.out.print("九九乘法表\n");
+        for(i=1;i<10;i++){
+            for(j=1;j<=i;j++){
+                k=i*j;
+                System.out.print(i+"×"+j+"="+k+"\t");
+            }
+            System.out.println(); // 这行代码的作用是换行，可以传空参数
+        }
     }
-    
-    
-    
-	}
-
 }
